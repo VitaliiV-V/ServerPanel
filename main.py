8307ce1c94002ea036e7b1b8b1b026d3e32489c9
master <mforcodeforces@rambler.ru>
2026-07-17 14:21:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,137 @@
+import uvicorn
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import psutil
+import socket
+import platform
+import time
+import datetime
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import subprocess
+from fastapi import Request
+import secrets
+from dotenv import load_dotenv
+import os
+
+load_dotenv("/home/master/Panel/.env")
+
+security = HTTPBasic()
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+def check_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_user = secrets.compare_digest(credentials.username, USERNAME)
+    correct_pass = secrets.compare_digest(credentials.password, PASSWORD)
+
+    if not (correct_user and correct_pass):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return True
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/main")
+def index(auth: bool = Depends(check_auth)):
+    return FileResponse("static/index.html")
+
+@app.get("/")
+def index():
+    return RedirectResponse(url="/main")
+
+def get_cpu_temp():
+    temps = psutil.sensors_temperatures()
+
+    for chip, entries in temps.items():
+        for e in entries:
+            # отбрасываем отключённые датчики
+            if e.current is not None and 0 < e.current < 120:
+                return e.current
+
+    return None
+
+def get_fan_speed():
+    try:
+        with open("/sys/class/hwmon/hwmon0/fan1_input") as f:
+            return int(f.read().strip())
+    except:
+        return None
+    
+@app.get("/info")
+def info():
+    boot_time = psutil.boot_time()
+    uptime_seconds = int(time.time() - boot_time)
+
+    return {
+        "cpu": psutil.cpu_percent(interval=0.5),
+        "fan" : get_fan_speed(),
+        "temp": get_cpu_temp(),
+        "ram": psutil.virtual_memory().percent,
+        "disk" : psutil.disk_usage('/').percent,
+        "hostname": socket.gethostname(),
+        "os": platform.system(),
+        "kernel": platform.release(),
+        "uptime": str(datetime.timedelta(seconds=uptime_seconds)),
+        "myip" : requests.get("https://api.ipify.org?format=json").json()["ip"],
+        "track" : "None",
+        "status" : "None", 
+        "progress" : 0, 
+        "position" : "None",
+        "length" : "None",
+        "volume" : 0
+    }
+
+@app.post("/exec")
+async def exec(request: Request):
+    data = await request.json()
+    print(data["command"])
+    if data["command"] == "poweroff":
+        subprocess.run(["poweroff"])
+    elif data["command"] == "reboot":
+        subprocess.run(["reboot"])
+    elif data["command"] == "suspend":        
+        subprocess.run(["systemctl", "suspend"])
+    elif data["command"] == "lock":
+        subprocess.run(["hyprlock"])
+    elif data["command"] == "playpause":
+        subprocess.run(['playerctl', 'play-pause'], capture_output=True, text=True)
+    elif data["command"] == "next":
+        subprocess.run(['playerctl', 'next'], capture_output=True, text=True)
+    elif data["command"] == "prev":
+        subprocess.run(['playerctl', 'previous'], capture_output=True, text=True)
+    return {
+        "message" : "ok"
+    }
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            uvicorn.run(
+                "main:app",
+                host="192.168.2.101",
+                port=8000,
+                reload=False
+            )
+            break
+        except Exception as e:
+            print(f"{e}")
+            time.sleep(3)
---
+++ static/app.js
@@ -0,0 +1,179 @@
+async function updateStats() { 
+
+    const response2 = await fetch("http://192.168.2.101:8000/info");
+    const data2 = await response2.json();
+    document.querySelector(".cpuval").textContent = data2.cpu + "%";
+    document.querySelector(".cpubar2").style.width = data2.cpu + "%";
+    document.querySelector(".ramval").textContent = data2.ram + "%";
+    document.querySelector(".rambar2").style.width = data2.ram + "%";
+    document.querySelector(".diskval").textContent = data2.disk + "%";
+    document.querySelector(".diskbar2").style.width = data2.disk + "%";
+    document.querySelector(".tempval").textContent = data2.temp + "°C";
+    document.querySelector(".fanval").textContent = data2.fan + " RPM";
+    document.querySelector(".tempbar2").style.width = data2.temp + "%";
+    document.querySelector(".fanbar2").style.width = 50 + "%";
+    document.querySelector(".valuehost").textContent = data2.hostname;
+    document.querySelector(".valueos").textContent = data2.os;
+    document.querySelector(".valuekernel").textContent = data2.kernel;
+    document.querySelector(".valuetime").textContent = data2.uptime;
+    document.querySelector(".trackname").textContent = data2.track;
+    document.querySelector(".position").textContent = data2.position;
+    document.querySelector(".length").textContent = data2.length;
+    document.querySelector(".vol").textContent = data2.volume + '%';
+
+    if(data2.status == "Playing\n") {
+        document.querySelector(".btn-play").innerHTML =
+          '<i class="fas fa-pause" style="font-size: 40px; padding-right: 5px;"></i>';   } else {
+        document.querySelector(".btn-play").innerHTML =
+          '<i class="fas fa-play" style="font-size: 40px;"></i>';
+    }
+    const slider = document.querySelector(".slider");
+
+    slider.value = data2.progress;
+
+    const volume = document.querySelector(".volume");
+
+    volume.value = data2.volume;
+
+    document.querySelector(".valueip").textContent = data2.myip;
+
+
+}
+
+
+setInterval(updateStats, 500);
+
+document.querySelector(".volume").addEventListener("change", function () {
+  console.log("Финальное значение:", slider.value);
+});
+
+document.querySelector(".reboot").addEventListener("click", async () => {
+
+    const ok = confirm("Are you sure you want to reboot your computer?");
+    if (!ok) return;
+
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "reboot",
+        })
+    })
+});
+
+
+document.querySelector(".suspend").addEventListener("click", async () => {
+
+    const ok = confirm("Are you sure you want to suspend your computer?");
+    if (!ok) return;
+
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "suspend",
+        })
+    })
+});
+
+document.querySelector(".poweroff").addEventListener("click", async () => {
+    const ok = confirm("Are you sure you want to turn off your computer?");
+
+    if (!ok) return;
+
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "poweroff",
+        })
+    })
+
+});
+
+document.querySelector(".lock").addEventListener("click", async () => {
+
+    const ok = confirm("Are you sure you want to lock your computer?");
+    if (!ok) return;
+
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "lock",
+        })
+    })
+});
+
+
+
+document.querySelector(".btn-play").addEventListener("click", async () => {
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "playpause",
+        })
+    })
+});
+
+document.querySelector(".btn-next").addEventListener("click", async () => {
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "next",
+        })
+    })
+});
+
+document.querySelector(".btn-prev").addEventListener("click", async () => {
+    const response = await fetch("http://192.168.2.101:8000/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "command": "previous",
+        })
+    })
+});
+
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    const icon = document.getElementById("themeIcon");
+    const isDark = document.body.classList.contains("dark");
+
+    if (isDark) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark") ? "dark" : "light"
+    );
+}
+
+window.onload = () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+};
